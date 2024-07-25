a0b34c8eb6103fd58268f7e588cbee6d2752cbb1
henrivilar <fabio.fv1@hotmail.com>
2024-07-25 19:54:52 -0300

    Adicionando funcionalidades aos botões

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const botaoPlayPause = document.getElementById('play-pause-bt')
 const faixaMusica = document.getElementById('faixa-musica')
+const btAvancar = document.getElementById('next-bt')
+const btVoltar = document.getElementById('previous-bt')
 
 const numeroMusicas = 23
 let taTocando = 0
+let faixaAtual = 1
 
 function tocarFaixa() {
     faixaMusica.play()
@@ -15,9 +18,37 @@ function pausarFaixa() {
 function tocarOuPausar () {
     if (taTocando === 0) {
         tocarFaixa()
+        taTocando = 1
     } else {
         pausarFaixa()
+        taTocando = 0
     }
 }
 
+function proximaFaixa () {
+    if (faixaAtual === numeroMusicas) {
+        faixaAtual = 1
+    } else {
+        faixaAtual += 1
+    }
+    
+    faixaMusica.src = "songs/" + faixaAtual + ".mp3"
+    tocarFaixa()
+    taTocando = 1
+}
+
+function voltarFaixa () {
+    if (faixaAtual === 1) {
+        faixaAtual = numeroMusicas
+    } else {
+        faixaAtual -= 1
+    }
+    
+    faixaMusica.src = "songs/" + faixaAtual + ".mp3"
+    tocarFaixa()
+    taTocando = 1
+}
+
 botaoPlayPause.addEventListener('click', tocarOuPausar)
+btAvancar.addEventListener('click', proximaFaixa)
+btVoltar.addEventListener('click', voltarFaixa)
